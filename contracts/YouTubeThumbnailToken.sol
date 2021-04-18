f847537cd7d7e59654ed2615a2de992124915516
Gnong <jinhongdev@gmail.com>
2021-04-18 22:39:00 +0900

    sol 파일 수정

--- contracts/YouTubeThumbnailToken.sol
+++ contracts/YouTubeThumbnailToken.sol
@@ -2,6 +2,6 @@ pragma solidity ^0.5.0;
 
 import "openzeppelin-solidity/contracts/token/ERC721/ERC721Full.sol";
 
-contract YouTubeThumbnailToken {
-  
+contract YouTubeThumbnailToken is ERC721Full{
+  constructor(string memory name, string memory symbol) ERC721Full(name, symbol) public {}
 }
